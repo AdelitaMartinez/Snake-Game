0b2c994080b4c9a8f33e397abfa90f72a09a6df9
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-01 13:23:17 -0600

    Drawing snake with JS

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 
 const board = document.getElementById('game-board')
 
+// Define game variables
+// 20 rows and 20 colums
+let snake = [{x: 10, y: 10}];
+
 // Draw game map, snake, and food
 function draw() {
   board.innerHTML = '';
+  drawSnake();
+}
+
+// Draw snake
+function drawSnake() {
+
 }
