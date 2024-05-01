227b1e7378f5dda5a922f6e7d9a49858e78f6437
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-01 13:25:03 -0600

    Creating game element

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // Define HTML elements
 
-const board = document.getElementById('game-board')
+const board = document.getElementById("game-board");
 
 // Define game variables
 // 20 rows and 20 colums
-let snake = [{x: 10, y: 10}];
+let snake = [{ x: 10, y: 10 }];
 
 // Draw game map, snake, and food
 function draw() {
-  board.innerHTML = '';
+  board.innerHTML = "";
   drawSnake();
 }
 
 // Draw snake
 function drawSnake() {
-
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement()
+  })
 }
