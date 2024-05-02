9904e79d00ee9a2f7523e7ef79451df09725b2b7
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-02 11:44:50 -0600

    Added up direction

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Define HTML elements
 
-const board = document.getElementById('game-board');
+const board = document.getElementById("game-board");
 
 // Define game variables
 // 20 rows and 20 colums
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
-let direction = 'right';
+let direction = "right";
 
 // Draw game map, snake, and food
 function draw() {
-  board.innerHTML = '';
+  board.innerHTML = "";
   drawSnake();
   drawFood();
 }
@@ -19,10 +19,10 @@ function draw() {
 // Draw snake
 function drawSnake() {
   snake.forEach((segment) => {
-    const snakeElement = createGameElement('div', 'snake')
-    setPosition(snakeElement, segment)
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
     board.appendChild(snakeElement);
-  })
+  });
 }
 
 // Create a snake or food cube/div
@@ -38,29 +38,38 @@ function setPosition(element, position) {
   element.style.gridRow = position.y;
 }
 
-// Testing Draw Function 
- draw();
+// Testing Draw Function
+draw();
 
 // Draw food function
 function drawFood() {
-  const foodElement = createGameElement('div', 'food');
+  const foodElement = createGameElement("div", "food");
   setPosition(foodElement, food);
-  board.appendChild(foodElement)
+  board.appendChild(foodElement);
 }
 
 // Generate food
 function generateFood() {
   const x = Math.floor(Math.random() * gridSize) + 1;
   const y = Math.floor(Math.random() * gridSize) + 1;
-  return {x, y};
+  return { x, y };
 }
 
-// Moving the snake 
+// Moving the snake
 function move() {
   const head = { ...snake[0] };
-  switch(direction) {
-    case 'right':
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "right":
       head.x++;
-        break;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "right":
+      head.x++;
+      break;
   }
 }
