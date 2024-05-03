c776ad789ba0593c81c251a1422445e2ee1f794c
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-03 09:52:07 -0600

    Made logic loop

--- script.js
+++ script.js
@@ -161,4 +161,11 @@ function checkCollision() {
   if( head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize) {
     resetGame();
   }
+
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      
+    }
+
+  }
 }
